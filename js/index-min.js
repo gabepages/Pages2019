b95591fe60d58b80edb95a893b8f2fae25b53e70
Gabe Pages <gpages@rentpath.com>
2019-03-27 11:20:38 -0400

    compressed

--- js/index-min.js
+++ js/index-min.js
@@ -1,68 +1,7 @@
-var $ = jQuery;
-$(document).ready(function () {
-
-
-  $(window).scroll(function () {
-    var distanceScrolled = $(this).scrollTop();
-    if (distanceScrolled > 60 || $(".menu").hasClass('open')) {
-      $('.menu').css('background', "#697061");
-    }
-  });
-
-  var $root = $('html, body');
-  $('a.scroll-tag').click(function () {
-    $(".menu.open").removeClass("open").addClass("closed");
-    if ($(this).attr('href')[0] == "#") {
-      event.preventDefault()
-      $root.animate({
-        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top - 80
-      }, 450);
-      return false;
-    }
-  });
-  // $(document).on('scroll', function () {
-  //   var scrollPosition = window.scrollY;
-  //   if (scrollPosition > $("a[name=contact]").offset().top - 150) {
-  //     $('#nav-contact').siblings().removeClass('active')
-  //     $('#nav-contact').addClass('active')
-  //   } else if (scrollPosition > $("a[name=portfolio]").offset().top - 61) {
-  //     $('#nav-portfolio').siblings().removeClass('active')
-  //     $('#nav-portfolio').addClass('active')
-  //   } else if (scrollPosition > $("a[name=services]").offset().top - 61) {
-  //     $('#nav-services').siblings().removeClass('active')
-  //     $('#nav-services').addClass('active')
-  //   } else if (scrollPosition > $("a[name=about]").offset().top - 61) {
-  //     $('#nav-about').siblings().removeClass('active')
-  //     $('#nav-about').addClass('active')
-  //   } else {
-  //     $('#nav-top').siblings().removeClass('active')
-  //     $('#nav-top').addClass('active')
-  //   }
-  // })
-  // if ($(window).width() <= 992) {
-  //   $(".services-wrapper").removeClass('valign-wrapper');
-  // }
-  // $(window).resize(function () {
-  //   if ($(window).width() <= 992) {
-  //     $(".services-wrapper").removeClass('valign-wrapper');
-  //   } else if (!$(".services-wrapper").hasClass('valign-wrapper')) {
-  //     $(".services-wrapper").addClass('valign-wrapper');
-  //   }
-  // })
-  $(".menu").on("click", function () {
-    if ($(".menu").hasClass('closed')) {
-      $(".menu.closed").removeClass("closed").addClass("open");
-    } else {
-      $(".menu.open").removeClass("open").addClass("closed");
-    }
-  });
-  $('#rsvp-form').on('submit', function () {
-    $(this).css('opacity', 0);
-    $('#form-submitted').css('opacity', 1)
-  })
-  // $('.modal').modal({
-  //   opacity: .6,
-  //   inDuration: 400,
-  //   outDuration: 300
-  // });
-});
+var $=jQuery;$(document).ready(function(){$(window).scroll(function(){var distanceScrolled=$(this).scrollTop();if(distanceScrolled>60||$(".menu").hasClass('open')){$('.menu').css('background',"#697061")}});var $root=$('html, body');$('a.scroll-tag').click(function(){$(".menu.open").removeClass("open").addClass("closed");if($(this).attr('href')[0]=="#"){event.preventDefault()
+  $root.animate({scrollTop:$('[name="'+$.attr(this,'href').substr(1)+'"]').offset().top-80},450);return!1}});$('#copyright').on('click',function(){$('.image-block img').each((index,element)=>{$(element).attr('src',refineImage(element))})})
+  function refineImage(element){var imageSrc=$(element).attr('src')
+  var splitImg=imageSrc.split('.')
+  splitImg[0]=splitImg[0]+'-refined'
+  return splitImg.join('.')}
+  $(".menu").on("click",function(){if($(".menu").hasClass('closed')){$(".menu.closed").removeClass("closed").addClass("open")}else{$(".menu.open").removeClass("open").addClass("closed")}});$('#rsvp-form').on('submit',function(){$(this).css('opacity',0);$('#form-submitted').css('opacity',1)})})
