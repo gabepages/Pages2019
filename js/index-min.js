f42f1d5ccd4ace8110ca0a613914d5ae1050f59d
Gabe Pages <gpages@rentpath.com>
2019-03-27 22:25:44 -0400

    fix bugs

--- js/index-min.js
+++ js/index-min.js
@@ -1,5 +1,7 @@
 var $=jQuery;$(document).ready(function(){$(window).scroll(function(){var distanceScrolled=$(this).scrollTop();if(distanceScrolled>60||$(".menu").hasClass('open')){$('.menu').css('background',"#697061")}});var $root=$('html, body');$('a.scroll-tag').click(function(){$(".menu.open").removeClass("open").addClass("closed");if($(this).attr('href')[0]=="#"){event.preventDefault()
-  $root.animate({scrollTop:$('[name="'+$.attr(this,'href').substr(1)+'"]').offset().top-80},450);return!1}});$('#copyright').on('click',function(){$('.image-block img').each((index,element)=>{$(element).attr('src',refineImage(element))})})
+  $root.animate({scrollTop:$('[name="'+$.attr(this,'href').substr(1)+'"]').offset().top-80},450);return!1}});$('#copyright').on('click',function(){if($('#copyright').hasClass('disabled'))return
+  $('#copyright').addClass('disabled')
+  $('.image-block img').each((index,element)=>{$(element).attr('src',refineImage(element))})})
   function refineImage(element){var imageSrc=$(element).attr('src')
   var splitImg=imageSrc.split('.')
   splitImg[0]=splitImg[0]+'-refined'
